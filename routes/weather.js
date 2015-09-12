a8166af27c3a1ecb2c0a285ed7f332ba43f1d388
aharter <git@andreas-harter.de>
2015-09-12 21:21:04 +0200

    Fetch Stockholm weather from yahoo weather api

--- routes/weather.js
+++ routes/weather.js
@@ -38,10 +38,33 @@ function queryOpenWeatherApi(response) {
   });
 }
 
+function queryYahooWeather(response) {
+  var query = 'select * from weather.forecast where u="c" and woeid in (select woeid from geo.places(1) where text="Stockholm")';
+
+  var yahooWeatherOptions = {
+    host: 'query.yahooapis.com',
+    port: 80,
+    path: '/v1/public/yql?format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=' + encodeURIComponent(query),
+    method: 'GET'
+  }
+
+  callWeatherApi(yahooWeatherOptions, function (buffer) {
+    var json = JSON.parse(buffer);
+    var result = {};
+    result.name = json.query.results.channel.location.city;
+    result.temperature = json.query.results.channel.item.condition.temp;
+    response(result);
+  });
+}
+
 /* GET weather for Stockholm */
 router.get('/', function (req, res, next) {
-  queryOpenWeatherApi(function (buffer) {
-    res.send(buffer);
+  queryOpenWeatherApi(function (openWeatherBuffer) {
+    queryYahooWeather(function (yahooWeatherBuffer) {
+      var finalBuffer = [];
+      finalBuffer.push(openWeatherBuffer, yahooWeatherBuffer)
+      res.send(finalBuffer);
+    });
   });
 });
 
