422e23fdb65a64bfb51dc7995bc8f7c83b0fc588
aharter <git@andreas-harter.de>
2015-09-12 20:08:23 +0200

    Fetch Stockholm weather from open weather api

--- routes/weather.js
+++ routes/weather.js
@@ -1,9 +1,48 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
+function callWeatherApi(requestOptions, callback) {
+  var buffer = '';
+
+  http.get(requestOptions, function (result) {
+    result.on('data', function (chunk) {
+      buffer += chunk;
+    });
+
+    result.on('end', function () {
+      callback(buffer);
+    });
+
+    result.on('error', function (e) {
+      throw 'An error occured ' + e;
+    });
+
+  });
+}
+
+function queryOpenWeatherApi(response) {
+  var openWeatherMapOptions = {
+    host: 'api.openweathermap.org',
+    port: 80,
+    path: '/data/2.5/weather?units=metric&q=Stockholm',
+    method: 'GET'
+  }
+
+  callWeatherApi(openWeatherMapOptions, function (buffer) {
+    var json = JSON.parse(buffer);
+    var result = {};
+    result.name = json.name;
+    result.temperature = json.main.temp;
+    response(result);
+  });
+}
+
 /* GET weather for Stockholm */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  queryOpenWeatherApi(function (buffer) {
+    res.send(buffer);
+  });
 });
 
 module.exports = router;
