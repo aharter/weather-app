465af7276173eac1eb37762a3c211f8b66a7c1c2
aharter <git@andreas-harter.de>
2015-09-13 14:55:48 +0200

    Fetch weather data from backend

--- public/controller/weather-app.js
+++ public/controller/weather-app.js
@@ -1,8 +1,29 @@
 (function () {
 	var app = angular.module('weatherApp', []);
-	app.controller('WeatherController', function() {
-		this.openWeatherTemp = 17;
-		this.yahooWeatherTemp = 19;
-		this.avgTemp = 18;
+	app.controller('WeatherController', function ($scope, $http) {
+		var weatherData = this;
+		$scope.city = '';
+		$scope.validCity = false;
+
+		function queryWeatherForCity(city) {
+			$http.get('/weather/' + encodeURIComponent(city)).then(
+				function (response) {
+					$scope.openWeatherTemp = Number(response.data[0].temperature);
+					$scope.yahooWeatherTemp = Number(response.data[1].temperature);
+					$scope.avgTemp = (($scope.openWeatherTemp + $scope.yahooWeatherTemp) / 2).toFixed(2);
+					$scope.validCity = true;
+					$scope.finalCity = response.data[1].name;
+				},
+				function (response) {
+					weatherData.validCity = false;
+				});
+		}
+
+		$scope.$watch('city', function (newValue, oldValue) {
+			if (newValue === '') {
+				return;
+			}
+			queryWeatherForCity(newValue);
+		});
 	});
 })();
